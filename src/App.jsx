5e7752fe0eb826d0598bb89b76f7c3b85031782e
Jack Michaud <mrmichaud42@gmail.com>
2020-05-30 10:49:03 -0700

    Adjust chin height

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ModuleContext from './contexts/module';
 const App = () => {
   const module = useContext(ModuleContext);
   return (
-    <div className="fixed gradient inset-x-0 bottom-0 uppercase text-black" style={{height: '1.5in'}}>
+    <div className="fixed gradient inset-x-0 bottom-0 uppercase text-black" style={{height: '1.4in'}}>
       <div className="flex items-center justify-center h-full">
         Family Home
       </div>
